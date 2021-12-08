913b43c18c9e096ce4576ccddbeacfdfb6f48e1e
Yu-Wei Chang <b01505025@g.ntu.edu.tw>
2021-12-09 00:08:37 +0800

    feat: static quantization tutorial code

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+data_path = '' # data path of ImageNet
+saved_model_dir = 'data/'
+float_model_file = 'mobilenet_v2-b0353104.pth'
+scripted_float_model_file = 'mobilenet_quantization_scripted.pth'
+scripted_quantized_model_file = 'mobilenet_quantization_scripted_quantized.pth'
